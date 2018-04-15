599ae4cfdcf11001222264cb7ecff67aabf8c44d
xiaolingzigit <121925495@qq.com>
2018-04-15 23:14:27 +0800

    【+】201804115

---
+++ Log4jAndCommons-logging/log Maven Webapp/src/main/java/testOne/TestOne.java
@@ -0,0 +1,12 @@
+package testOne;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class TestOne {
+	private static Log log = LogFactory.getLog(TestOne.class);
+	public static void main(String[] args) {
+		log.debug("hahah");
+		log.info("hihi");
+	}
+}
